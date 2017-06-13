ab58087f3d98e9b222563e7d8d4add7b6d3f3da7
Unknown <wl9@indiana.edu>
2017-06-13 12:23:33 -0700

    added parser

--- scripts/parser.py
+++ scripts/parser.py
@@ -4,23 +4,37 @@ from nltk.parse.stanford import StanfordDependencyParser
 path_prefix = "/Users/w2li/Documents/nli/data/essays/train"
 parser_path = "/usr/local/Cellar/stanford-parser/3.6.0/bin/lexparser.sh"
 
-in_path = path_prefix + "/original"
+in_path = path_prefix + "/tokenized"
 out_path = path_prefix + "/parsed"
 
 os.environ['STANFORD_PARSER'] = '/usr/local/Cellar/stanford-parser/3.6.0/libexec'
 os.environ['STANFORD_MODELS'] = '/usr/local/Cellar/stanford-parser/3.6.0/libexec'
 
-# dep_parser = StanfordDependencyParser()
 dep_parser = StanfordDependencyParser(model_path = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
 
-for dep_graphs in dep_parser.raw_parse_sents(open("../data/essays/train/tokenized/00001.txt").readlines()):
-    for parse in dep_graphs:
-        for i in parse.triples():
-                print(i)
-    print()
-# for subdir, dirs, files in os.walk(in_path):
-#     for filename in files:
-#         in_filename = os.path.join(subdir, filename)
-#         out_filename = os.path.join(out_path, filename)
-#         print("Parsing {} ...".format(in_filename))
-#         os.system("{} {} > {}".format(parser_path, in_filename, out_filename))
+for subdir, dirs, files in os.walk(in_path):
+    for filename in files:
+        if int(filename[:5]) > 70:
+            in_filename = os.path.join(subdir, filename)
+            out_filename = os.path.join(out_path, filename)
+            print("Parsing {} ...".format(in_filename))
+            try:
+                f = open(in_filename)
+                f_writer = open(out_filename, "w")
+
+                for dep_graphs in dep_parser.parse_doc(f.read()):
+                    for parse in dep_graphs:
+                        for i in parse.triples():
+                            if len(i) == 3:
+                                f_writer.write("_".join([i[0][0], i[1], i[2][0]]))
+                                f_writer.write("\n")
+                                f_writer.write("_".join([i[0][1], i[1], i[2][0]]))
+                                f_writer.write("\n")
+                                f_writer.write("_".join([i[0][0], i[1], i[2][1]]))
+                                f_writer.write("\n")
+                                f_writer.write("_".join([i[0][1], i[1], i[2][1]]))
+                                f_writer.write("\n")
+                f.close()
+                f_writer.close()
+            except Exception as e:
+                print(e)
